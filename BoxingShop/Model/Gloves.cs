ade6f1dd92ed716143317ca3ab9271e58c25629b
wh1teMen <aleexandr777@gmail.com>
2023-12-30 14:42:27 +0500

    Добавьте файлы проекта.

---
+++ BoxingShop/Model/Gloves.cs
@@ -0,0 +1,13 @@
+﻿namespace BoxingShop.Model
+{
+    public class Gloves
+    {
+        public int Id { get; set; }
+        public string? Brand { get; set; }
+        public string? Color {  get; set; }
+        public int Size {  get; set; }
+        public string? Description {  get; set; }
+        public decimal Price {  get; set; }
+        public string? UrlImg {  get; set; }
+    }
+}
